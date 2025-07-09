485809bbfdacf0b362076d4c8b6809f74d2547e0
sachin <sachinp@iitbhilai.ac.in>
2025-07-09 10:44:57 +0530

    added app folder and a health check endpoint

--- app/main.py
+++ app/main.py
@@ -9,3 +9,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/health")
+def health_check():
+    """
+    Health check endpoint
+    """
+    return {"status": "healthy"}
